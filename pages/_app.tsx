486d701955f7d9176cc77a4755648277e8b2c404
Haikhal Fakhreez <ekaliacid@Ekals-MacBook-Pro.local>
2022-08-21 02:00:02 +0800

    update meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,12 +28,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:description" content={WEBSITE_DESCRIPTION} />
         <meta property="og:image" content={WEBSITE_IMAGE} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={WEBSITE_TITLE} />
-        <meta name="twitter:description" content={WEBSITE_DESCRIPTION} />
         <meta name="twitter:site" content="@ekaliacid" />
         <meta name="twitter:creator" content="@ekaliacid" />
-        <meta name="twitter:image" content={WEBSITE_IMAGE} />
-        <meta name="twitter:alt" content={WEBSITE_TITLE} />
 
         <link rel="canonical" href={WEBSITE_URL + router.pathname} />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
